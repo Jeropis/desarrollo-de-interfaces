97aca448a0b77b54abf0877e07598c8637e2a39d
Jeropis <jerocaru@gmail.com>
2022-12-12 19:30:16 +0100

    Ultimando proyecto

--- prLibros/src/application/DatabaseConnection.java
+++ prLibros/src/application/DatabaseConnection.java
@@ -11,7 +11,7 @@ public class DatabaseConnection {
 	public Connection getConnection () {
 		String dbName = "bd_libros";
 		String userName = "root";
-		String password = "admin";
+		String password = "root";
 		
 		try {
 			Class.forName("com.mysql.cj.jdbc.Driver");
